0455a87902f7b79add9862335bac9998554103e7
maartjeth <maartjeterhoeve@gmail.com>
2016-04-13 15:14:35 +0200

    parameters ibm smooth outside function

--- Project1/src/ibm1_smooth.py
+++ Project1/src/ibm1_smooth.py
@@ -1,12 +1,13 @@
-from ibm import IBM1
+from ibm1 import IBM1
 
 class IBM1_SMOOTH(IBM1):
 
-  def _update_parameters(self, params, joint_expectations, expectations, n, V):
+  n = 1
+  V = 50000
+
+  def _update_parameters(self, params, joint_expectations, expectations):
     for e in expectations:
       expectation_e = expectations[e]
       joint_expectations_e = joint_expectations[e]
       for f in joint_expectations_e:
         params[f][e] = (joint_expectations_e[f] + n) / (expectation_e + n * V)
-
-
