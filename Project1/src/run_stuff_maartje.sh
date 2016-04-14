9e2878d2dae295c76f3cfdedcfdc9166862a2de4
maartjeth <maartjeterhoeve@gmail.com>
2016-04-14 14:24:47 +0200

    run imb2 uniform + random1, plots, script for making ll plots

---
+++ Project1/src/run_stuff_maartje.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# All models that need to be run are available here
+# Make sure the paramaters are in order french english french english
+# run as "sh run_stuff.sh" or select just pick one and run it in a terminal
+
+echo "Training IBM1"
+python main.py ibm1 C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/training/hansards.36.2.f C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/training/hansards.36.2.e C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/test/test.f C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/test/test.e C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/answers/test.wa.nonullalign 10 ibm1.pkl > ibm1.log
+
+echo "Training IBM1 with null words"
+python main.py ibm1_add0 C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/training/hansards.36.2.f C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/training/hansards.36.2.e C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/test/test.f C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/test/test.e C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/answers/test.wa.nonullalign 10 ibm1_add0.pkl > ibm1_add0.log
+
+echo "Training IBM1 with smoothing"
+python main.py ibm1_smooth C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/training/hansards.36.2.f C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/training/hansards.36.2.e C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/test/test.f C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/test/test.e C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/answers/test.wa.nonullalign 10 ibm1_smooth.pkl > ibm1_smooth.log
+
+echo "Training IBM2 uniform"
+python main.py ibm2 C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/training/hansards.36.2.f C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/training/hansards.36.2.e C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/test/test.f C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/test/test.e C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/answers/test.wa.nonullalign 10 ibm2_uniform.pkl uniform > ibm2_uniform.log
+
+echo "Training IBM2 random1"
+python main.py ibm2 C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/training/hansards.36.2.f C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/training/hansards.36.2.e C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/test/test.f C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/test/test.e C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/answers/test.wa.nonullalign 10 ibm2_random1.pkl random > ibm2_random1.log
+
+echo "Training IBM2 random2"
+python main.py ibm2 C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/training/hansards.36.2.f C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/training/hansards.36.2.e C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/test/test.f C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/test/test.e C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/answers/test.wa.nonullalign 10 ibm2_random2.pkl random > ibm2_random2.log
+
+echo "Training IBM2 random3"
+python main.py ibm2 C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/training/hansards.36.2.f C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/training/hansards.36.2.e C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/test/test.f C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/test/test.e C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/answers/test.wa.nonullalign 10 ibm2_random3.pkl random > ibm2_random3.log
+
+echo "Training IBM2 ibm1"
+python main.py ibm2 C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/training/hansards.36.2.f C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/training/hansards.36.2.e C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/test/test.f C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/test/test.e C:/Users/Maartje/Documents/Studie/master/nlp2/project1/NLP2_Project1_data/NLP2_Project1_data/testing/answers/test.wa.nonullalign 10 ibm2_ibm1.pkl ibm1 ibm1.pkl > ibm2_ibm1.log
+
